e1ccd268e86986b69ace32d82cf0042cff199e3f
ErdemHHO <75128279+ErdemHHO@users.noreply.github.com>
2022-10-27 00:28:21 +0300

    güncelleme

--- data/db.js
+++ data/db.js
@@ -1,14 +1,30 @@
 const mysql = require("mysql2");
 const config = require("../config/config.js");
 
-let connection = mysql.createConnection(config.db);
-
-connection.connect(function(err) {
-    if(err) {
-        console.log(err);
+const Sequelize=require("sequelize");
+const sequelize=new Sequelize(config.db.database,config.db.user,config.db.password,{
+    dialect:"mysql",
+    host:config.db.host
+})
+async function connect(){
+    try{
+    await sequelize.authenticate();
+    console.log("msqlserver bağlantısı başarılı")
     }
+    catch(err){
+        console.log("bağlantı hatası",err);
+    }
+}
+connect();
+module.exports=sequelize; 
+// let connection = mysql.createConnection(config.db);
 
-    console.log("mysql bağlantısı yapıldı");
-});
+// connection.connect(function(err) {
+//     if(err) {
+//         console.log(err);
+//     }
 
-module.exports = connection.promise();
+//     console.log("mysql bağlantısı yapıldı");
+// });
+
+// module.exports = connection.promise();
--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ router.get('/profil',authController.profil);
 router.get('/kullaniciislem',authController.kullaniciIslem);
 router.get('/login',authController.loginFormunuGoster);
 router.post('/login',authController.login)
+
 // router.get('/',authController.AnasayfaGoster);
 
 router.get("/stajimeislemleri", async function(req, res) {
@@ -36,23 +37,52 @@ router.get("/kullaniciislemleri", async function(req, res) {
         console.log(err);
     }
 });
+router.get("/staj", async function(req, res) {
+    try {
+        const [products, ] = await db.execute("select * from kullanici");
+        const [rol, ] = await db.execute("select * from rol");
+        res.render("ogrenci/staj1.ejs", {
+            kullaniciTable: products,
+            rolTable:rol
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
+router.get("/ogretmensayfasi", async function(req, res) {
+    try {
+        res.render("ogretmen/ogretmenSayfasi.ejs", {
+        });
+    }
+    catch(err) {
+        console.log(err);
+    }
+});
 router.post("/kullaniciislemleri", async function(req, res){
 
     const kullaniciAd=req.body.kullaniciAd;
     console.log(req.body.kullaniciAd);
     const kullaniciSoyad=req.body.kullaniciSoyad;
+    console.log(req.body.kullaniciSoyad);
     const kullaniciNumara=req.body.kullaniciNumara;
+    console.log(req.body.kullaniciNumara);
     const kullaniciParola=req.body.kullaniciParola;
+    console.log(req.body.kullaniciParola);
     const kullaniciTelNo=req.body.kullaniciTelNo;
+    console.log(req.body.kullaniciTelNo);
     const kullaniciMail=req.body.kullaniciMail;
+    console.log(req.body.kullaniciMail);
     const kulRol=req.body.kulRol;
-    if(kulRol==1){
-    }
+    console.log(req.body.kulRol);
     const kullaniciFakulte=req.body.kullaniciFakulte;
+    console.log(req.body.kullaniciFakulte);
     const kullaniciBolum=req.body.kullaniciBolum;
+    console.log(req.body.kullaniciBolum);
     const kullaniciSinif=req.body.kullaniciSinif;
+    console.log(req.body.kullaniciSinif);
     try{
-      await db.execute("INSERT INTO kullanici(kullaniciNumara,kullaniciAd,kullaniciSoyad,kullaniciParola,kullaniciMail,kullaniciTelNo,kullaniciFakulte,kullaniciBolum,kullaniciSinif,rolID) VALUES(?,?,?,?,?,?,?,?,?,?)",[kullaniciNumara,kullaniciAd,kullaniciSoyad,kullaniciParola,kullaniciMail,kullaniciTelNo,kullaniciFakulte,kullaniciBolum,kullaniciSinif,rolID])  
+      await db.execute("INSERT INTO kullanici(kullaniciNumara,kullaniciAd,kullaniciSoyad,kullaniciParola,kullaniciMail,kullaniciTelNo,kullaniciFakulte,kullaniciBolum,kullaniciSinif,rolID) VALUES(?,?,?,?,?,?,?,?,?,?)",[kullaniciNumara,kullaniciAd,kullaniciSoyad,kullaniciParola,kullaniciMail,kullaniciTelNo,kullaniciFakulte,kullaniciBolum,kullaniciSinif,kulRol])  
     }
     catch(err){
         console.log(err)
@@ -60,10 +90,8 @@ router.post("/kullaniciislemleri", async function(req, res){
 })
 router.post("/duyuruolustur", async function(req, res){
     const duyuruBaslik=req.body.duyuruBaslik;
-    console.log(req.body.duyuruBaslik);
     const duyuruAciklama=req.body.duyuruAciklama;
     const duyuruTuru=req.body.duyuruTuru;
-    console.log(req.body.duyuruAciklama);
     try{
       await db.execute("INSERT INTO duyuru(duyuruBaslik,duyuruAciklama,duyuruTuru) VALUES(?,?,?)",[duyuruBaslik,duyuruAciklama,duyuruTuru])  
     }
@@ -75,7 +103,6 @@ router.get("/", async function(req, res) {
     try {
         const [duyurular, ] = await db.execute("select * from duyuru");
         let diziUzunluk=duyurular.length;
-        console.log(duyurular[0]);
         res.render("sayfalar/homepage.ejs", {
             duyuru:duyurular,
             diziUzunluk:diziUzunluk
