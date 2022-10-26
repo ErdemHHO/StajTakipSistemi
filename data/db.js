142bec3f0d713b2e351f20c74c6488fa9b33e074
ErdemHHO <75128279+ErdemHHO@users.noreply.github.com>
2022-10-27 01:22:52 +0300

    sequelize kaldırıldı inş lazım olmaz tekrar

--- data/db.js
+++ data/db.js
@@ -1,30 +1,30 @@
 const mysql = require("mysql2");
 const config = require("../config/config.js");
 
-const Sequelize=require("sequelize");
-const sequelize=new Sequelize(config.db.database,config.db.user,config.db.password,{
-    dialect:"mysql",
-    host:config.db.host
-})
-async function connect(){
-    try{
-    await sequelize.authenticate();
-    console.log("msqlserver bağlantısı başarılı")
-    }
-    catch(err){
-        console.log("bağlantı hatası",err);
-    }
-}
-connect();
-module.exports=sequelize; 
+// const Sequelize=require("sequelize");
+// const sequelize=new Sequelize(config.db.database,config.db.user,config.db.password,{
+//     dialect:"mysql",
+//     host:config.db.host
+// })
+// async function connect(){
+//     try{
+//     await sequelize.authenticate();
+//     console.log("msqlserver bağlantısı başarılı")
+//     }
+//     catch(err){
+//         console.log("bağlantı hatası",err);
+//     }
+// }
+// connect();
+// module.exports=sequelize; 
 // let connection = mysql.createConnection(config.db);
 
-// connection.connect(function(err) {
-//     if(err) {
-//         console.log(err);
-//     }
+connection.connect(function(err) {
+    if(err) {
+        console.log(err);
+    }
 
-//     console.log("mysql bağlantısı yapıldı");
-// });
+    console.log("mysql bağlantısı yapıldı");
+});
 
-// module.exports = connection.promise();
+module.exports = connection.promise();
